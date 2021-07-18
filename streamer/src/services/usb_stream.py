24d1a6b7860e4e6077bec9afb4e90f4c83c84504
Jack <jack.mead@foreveroceans.com>
2021-07-18 13:25:01 -1000

    settings

--- streamer/src/services/usb_stream.py
+++ streamer/src/services/usb_stream.py
@@ -40,11 +40,10 @@ def get_tuned_encoding_pipeline(name: str, options: dict):
 
     # -vprofile baseline \
     # -fflags nobuffer \
-    #  
+    # -vf "drawtext=text='%{{localtime\: {name} --- %m/%d/%Y %I.%M.%S %p}}':fontsize={options.get('fontsize')}:fontcolor=yellow@0.8:x=10:y=10:shadowcolor=blue@0.6:shadowx=2:shadowy=2" \
 
     return f"""\
     -vcodec {options.get('vcodec')} \
-    -vf "drawtext=text='%{{localtime\: {name} --- %m/%d/%Y %I.%M.%S %p}}':fontsize={options.get('fontsize')}:fontcolor=yellow@0.8:x=10:y=10:shadowcolor=blue@0.6:shadowx=2:shadowy=2" \
     -preset veryfast \
     -tune zerolatency \
     -pix_fmt yuv420p \
